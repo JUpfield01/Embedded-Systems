35f671791308082cdab3ae0f17c505b80476c551
James Upfield <j.upfield01@gmail.com>
2022-10-11 19:06:02 +0100

    Finished Task 252 Challenge
    All Level 5 Labs completed - Woop!

--- Tasks/Task-252-SerialInterrupts/main.cpp
+++ Tasks/Task-252-SerialInterrupts/main.cpp
@@ -1,4 +1,5 @@
 #include "uop_msb.h"
+#include "mbed.h"
 #include <chrono>
 #include <ratio>
 using namespace uop_msb;
@@ -20,6 +21,9 @@ Ticker ledTicker;
 //Flash rate (us)
 volatile long long T = 500000;
 
+char prevChar;
+int strChecker=0;
+
 //ISR for flashing blue LED
 void onTick()
 {
@@ -34,37 +38,109 @@ void on_rx_interrupt()
     // Read the data to clear the receive interrupt.
     if (serial_port.read(&p, 1)) {
 
+        //p=toLower(p);
         //Check the character input
         switch (p) {
         case 13:
             //User pressed return
             serial_port.write("\n\r",2); //Echo a newline
+            strChecker=0;
             break;
+
         case '1':
             //Switch on buzzer
             buzz.playTone("A");
+            strChecker=0;
             break;
+
         case '2':
             //Switch off buzzer
             buzz.rest();
+            strChecker=0;
             break;
+
         case '+':
             T += 100000;
             ledTicker.detach();
             ledTicker.attach(&onTick, microseconds(T));
+            strChecker=0;
             break;
+
         case '-':
             if (T >= 200000) {
                 T -= 100000;
                 ledTicker.detach();
                 ledTicker.attach(&onTick, microseconds(T));
             }
-            break;            
+            strChecker=0;
+            break;
+
+        case 'u':
+        case 'U':
+        //case 'P':
+            strChecker=2;
+            //led1 = !led1;
+            break; 
+
+        case 'p':
+        case 'P':
+        //case 'P':
+            if((prevChar=='u'||'U')&&(strChecker==2)) {
+                T += 100000;
+                ledTicker.detach();
+                ledTicker.attach(&onTick, microseconds(T));
+            }  
+            strChecker=0;
+            break; 
+
+        case 'd':
+        case 'D':
+                strChecker=1;
+                //led1 = !led1;
+            break;
+
+        case 'o':
+        case 'O':
+            if((prevChar=='d'||'D')&&(strChecker==1)) {
+                strChecker=1;
+                //led1 = !led1;
+            }
+            else {
+                strChecker=0;
+            }
+            break;
+
+        case 'w':
+        case 'W':
+            if((prevChar=='o'||'O')&&(strChecker==1)) {
+                strChecker=1;
+                //led1 = !led1;
+            }
+            else {
+                strChecker=0;
+            }
+            break;
+
+        case 'n':
+        case 'N':
+            if((prevChar=='w'||'W')&&(strChecker==1)) {
+                //led1 = !led1;
+                if (T >= 200000) {
+                    T -= 100000;
+                    ledTicker.detach();
+                    ledTicker.attach(&onTick, microseconds(T));
+                }
+            }
+            strChecker=0;
+            break;
+
         default:
             //Echo typed character to the terminal
-            serial_port.write(&p,1);
+            strChecker=0;
             break;
         };
+        serial_port.write(&p,1);
+        prevChar=p;
     }
 
     // Toggle the LED.
@@ -73,14 +149,20 @@ void on_rx_interrupt()
 
 LCD_16X2_DISPLAY disp;
 
+
+
+
 int main(void)
 {
     // Set desired properties (115200-8-N-1).
     serial_port.baud(115200);
     serial_port.format(
-        /* bits */ 8,
-        /* parity */ SerialBase::None,
-        /* stop bit */ 1
+
+        /* bits */ //8,
+        /* parity */ //SerialBase::None,
+        /* stop bit */ //1
+
+
     );
 
     // Register a callback to process a Rx (receive) interrupt.
@@ -94,7 +176,26 @@ int main(void)
     while (true) {
         sleep(); 
         disp.cls();
-        disp.printf("T=%Ldms", T/1000);    
+        disp.locate(1,1);
+        disp.printf("T=%Ldms", T/1000);
+        disp.locate(2,1);
+        disp.printf("str: %c", prevChar);
+        disp.locate(2,8);
+        disp.printf("str: %d", strChecker);
+        //printf("T=%Ldms", T);    
     }
     
 }
+
+
+/*
+int main(void)
+{
+    while(true) {
+        printf("Test\n");
+        wait_us(1000000);
+    }
+    
+}
+
+*/
